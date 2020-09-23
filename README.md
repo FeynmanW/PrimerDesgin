584bcd715016911dca4e49700eda0ab650194ead
Bo Wang <feynmanw@126.com>
2020-09-23 12:14:40 +0800

    Fisrt upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# PrimerDesign
+PrimerDesign is a small tool in order to avoid repeated operations which is frustating and time-consuming.
+## primer.R
+This is an R script to concatenate two sequences: one is a 25bp overlap sequence for Gibson assembly, and the other is a sequence about 20bp whose length is adjusted based on Tm (melting temperature). This program can be used as a command line tool in a UNIX system, providing the necessary parameters. And the output file is a `.csv` file containing primer sequence generated.
+
+```shell
+Usage: primer.R [options]
+
+Options:
+	-o CHARACTER, --output=CHARACTER
+		Set the prefix of output file name
+
+	-n INTEGER, --number=INTEGER
+		Number of primers to design
+
+	-i CHARACTER, --input=CHARACTER
+		Input file consist of overlap sequence for Gibson assembly and reference sequence
+
+	-g NUMERIC, --gap=NUMERIC
+		Selection gap for insertion sites [default= 1]
+
+	-h, --help
+		Show this help message and exit
+```
+
+### Usage example
+
+```shell
+Rscript primer.R -o EP3_ICL3 -g 1 -n 10 -i input.csv
+```
+
---
+++ primer.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+# Title: Automatic construction of Primers for Sequence Insertion ####
+# Author: Wang Bo https://github.com/FeynmanW ####
+library("optparse")
+
+# Parsing Arguments ############################################################
+option_list = list(
+  make_option(c("-o", "--output"), type="character", default = "output_primer",
+              help="Set the prefix of output file name", metavar="character"),
+  make_option(c("-n", "--number"), type="integer", default = 1,
+              help="Number of primers to design", metavar="integer"),
+  make_option(c("-i", "--input"), type="character", default = NULL,
+              help="Input file consist of overlap sequence for Gibson assembly and reference sequence", metavar="character"),
+  make_option(c("-g", "--gap"), type="integer", default = 1,
+              help="Selection gap for insertion sites [default= %default]", metavar="numeric")
+)
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser)
+
+inputfile <- opt$input
+gap <- opt$gap
+n <- opt$number
+outputfile <- opt$output
+
+calculateGCcontent <- function(dnaSeq){
+  dnaSeq <- strsplit(toupper(dnaSeq), split = "")[[1]]
+  len <- length(dnaSeq)
+  count <- 0
+  for (i in 1:len) {
+    if (dnaSeq[i] == "G" | dnaSeq[i] == "C") {
+      count <- count + 1
+    }
+  }
+  count / len * 100
+}
+
+calculateTm <- function(dnaSeq){
+  GCcontent <- calculateGCcontent(dnaSeq)
+  dnaSeq <- strsplit(toupper(dnaSeq), split = "")[[1]]
+  len <- length(dnaSeq)
+  69.3 + 0.41 * GCcontent - 650/len
+}
+
+refSeq <- read.csv(inputfile, header = F)
+for (i in 1:nrow(refSeq)) {
+  overlap <- toupper(refSeq[i, 1])
+  ref <- refSeq[i, 2]
+  primer <- data.frame(sequence=character(), stringsAsFactors = FALSE)
+  for (j in 1:n){
+    linkPrimer <- substr(ref, (j-1)*gap*3+1, (j-1)*gap*3+20)
+    if (calculateTm(linkPrimer) > 65) linkPrimer <- substr(ref, (j-1)*gap*3+1, (j-1)*gap*3+18)
+    primer[j,1] <- paste0(overlap, linkPrimer)
+  }
+  write.table(primer, file = paste0(outputfile, "_", i, ".csv"), sep = ",", quote = F, row.names = F, col.names = T)
+}
